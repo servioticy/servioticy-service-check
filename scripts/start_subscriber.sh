6b575db8d636dc53a311fbdae94414d45593f319
David Carrera <david.carrera@bsc.es>
2015-08-28 00:46:12 +0200

    revised indenting of subscriber

--- scripts/start_subscriber.sh
+++ scripts/start_subscriber.sh
@@ -7,7 +7,7 @@ do
 
   id=`cat $1/$IDS_FOLDER/$file.id | perl -pe "s/\"/\n/g" | head -4 | tail -1`
   rm -rf $TMPDIR/$SAMPLE_SUBSCRIPTION_DATA
-  echo "    Subscribing to topic /topic/"$at"."$id".streams."$SAMPLE_STREAM".updates"
+  echo "      Subscribing to topic /topic/"$at"."$id".streams."$SAMPLE_STREAM".updates"
   $NODE_HOME/bin/node $SCRIPTS/subscriber.js $at $id $SAMPLE_STREAM > $TMPDIR/$SAMPLE_SUBSCRIPTION_DATA &
   
   sleep 1
